5c999cbeb81a0dc6805d466dde9e65f3fb0e4d8e
WThiersLucena <thiers_@hotmail.com>
2022-02-17 16:39:29 -0300

    1 Up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css';
+import Primeiro from './components/Primeiro';
+import Segundo from './components/Segundo';
+
+function App() {
+  return (
+  <Primeiro></Primeiro>,
+  <Segundo></Segundo>    
+    )
+  
+  }
+
+export default App;
---
+++ src/components/Segundo.jsx
@@ -0,0 +1,3 @@
+
+
+export default() => <h2>Primeiro dentro de components</h2>
