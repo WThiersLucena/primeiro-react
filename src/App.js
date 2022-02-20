30973df122f0f8ac3bb65cb81bb463c9537e2326
WThiersLucena <thiers_@hotmail.com>
2022-02-19 23:54:46 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react'
-import './App.css';
+import React from 'react';
 import Primeiro from './components/Primeiro';
-import Segundo from './components/Segundo';
-import Teste ,{Component1,Component2} from './components/Multi';
-
+import CompPadrao, {Component1, Component2} from './components/Multi.js';
+import Estilos from './components/Estilos';
+import MinMax from './components/MinMax';
+import Titulo from './components/Titulo';
+import Botao from './components/Botao';
 
 
 function App() {
+
   return (
-    <div>
-      <h2> Teste dentro de App.js</h2>
-      <Primeiro></Primeiro>
-      <Segundo></Segundo> 
-      <Teste></Teste>
-      <Component1></Component1>
-      <Component2></Component2>
-      </div>   
-    )
-  
-  }
+   <div>
+    <Primeiro />
+    <CompPadrao />
+    <Component1 />
+    <Component2 />
+    <Estilos />
+    <MinMax min="10" max="20" />
+    <MinMax min={5} max={10} />
+    <Titulo principal="1º [ Olá ]" secundaria="2º [ Olá ]" />
+    <Botao />
+  </div>
+);
+}
 
 export default App;
---
+++ src/components/MinMax.js
@@ -0,0 +1,10 @@
+const MinMax = (props) => {
+    console.log(props)
+     return (
+    <h2>
+        O valor de {props.max} é maior que o valor de {props.min}
+    </h2>
+    )
+};
+
+export default MinMax;
